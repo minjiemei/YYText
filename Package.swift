eed194f8dca8895fc1e1d6737f25bce23d97b676
minjiemei <minjie.mei@beesoft.io>
2025-09-17 17:17:13 +0800

    no message

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "YYText",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "YYText",
+            targets: ["YYText"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "YYText"
+        ),
+        .testTarget(
+            name: "YYTextTests",
+            dependencies: ["YYText"]
+        ),
+    ]
+)

b34ae9ea75f2e81bf888affa9a0a6affe986933b
minjiemei <minjie.mei@beesoft.io>
2025-09-18 10:09:19 +0800

    no message

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,15 +9,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "YYText",
-            targets: ["YYText"]
-        ),
+            targets: ["YYText"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "YYText"
-        ),
+            name: "YYText",
+            path: "Sources",
+            publicHeadersPath: "include/."),
         .testTarget(
             name: "YYTextTests",
             dependencies: ["YYText"]
--- Tests/YYTextTests/YYTextTests.swift
+++ Tests/YYTextTests/YYTextTests.swift
@@ -1,6 +1,11 @@
 import Testing
 @testable import YYText
 
+@MainActor
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    let label = YYLabel()
+    label.text = ""
+    label.backgroundColor = UIColor.red
+    print(label)
 }
